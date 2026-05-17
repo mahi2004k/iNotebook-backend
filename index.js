3cef8bba1031b8883076c850052e01349d99f83c
mahi2004k <mkonnur718@gmail.com>
2026-05-17 11:35:01 +0530

    Changed backend url after deploying backend

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const connectToMongo = require("./db");
-const express = require('express')
+const express = require('express');
 const cors = require("cors");
-const app = express()
-const port = 5000
+
+const app = express();
+const port = process.env.PORT || 5000;
+
 connectToMongo();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
-
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
-  console.log(`iNotebook backend listening on port http://localhost:${port}`)
-})
-
+  console.log(`iNotebook backend listening on port ${port}`);
+});
